202f0648b6b97838276d6d525007d748ba5a8027
Toby Inkster <mail@tobyinkster.co.uk>
2020-10-09 23:39:29 +0100

    bump version; changelog

--- lib/Sub/MultiMethod.pm
+++ lib/Sub/MultiMethod.pm
@@ -5,7 +5,7 @@ use warnings;
 package Sub::MultiMethod;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.8';
+our $VERSION   = '0.900';
 
 use B ();
 use Exporter::Shiny qw( multimethod multimethods_from_roles monomethod );
